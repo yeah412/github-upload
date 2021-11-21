b3c62631b0942ada7be065733df2129c347f1475
yeah412 <yeah412@hotmail.com>
2021-11-21 21:06:41 +0300

    add new features + makefile

--- classPerson_Src.cpp
+++ classPerson_Src.cpp
@@ -4,17 +4,39 @@
 
 #include "classPerson.hpp"
 
+int Person::_peopleCount = 0;
+
 using namespace std;
 
-Person::Person():_name("unknown"), _surname("unknown"), _age(-1){}
+Person::Person():_name("unknown"), _surname("unknown"), _age(-1){
 
-Person::Person(const string &n):_name(n), _surname("unknown"), _age(-1){}
+    _peopleCount++;
+}
 
-Person::Person(const int &a):_name("unknown"), _surname("unknown"), _age(a){}
+Person::Person(const string &n):_name(n), _surname("unknown"), _age(-1){
 
-Person::Person(const string &n, const string &sn):_name(n), _surname(sn), _age(-1){}
+    _peopleCount++;
+}
 
-Person::Person(const string &n, const string &sn, const int &a):_name(n), _surname(sn), _age(a){}
+Person::Person(const int &a):_name("unknown"), _surname("unknown"), _age(a){
+
+    _peopleCount++;
+}
+
+Person::Person(const string &n, const int &a):_name(n), _surname("unknown"), _age(a){
+
+    _peopleCount++;
+}
+
+Person::Person(const string &n, const string &sn):_name(n), _surname(sn), _age(-1){
+
+    _peopleCount++;
+}
+
+Person::Person(const string &n, const string &sn, const int &a):_name(n), _surname(sn), _age(a){
+
+    _peopleCount++;
+}
 
 const string Person::getName()  const{
 
@@ -53,4 +75,21 @@ ostream& operator<<(ostream &out, const Person &p){
     out << p.getAge() << endl;
 
     return out;
+}
+
+void Person::output(){
+
+    cout << _name << endl;
+    cout << _surname << endl;
+    cout << _age << endl;
+}
+
+int Person::operator+(const Person &other){
+
+    return _age + other.getAge();
+}
+
+int Person::getPeopleCount(){
+
+    return _peopleCount;
 }
